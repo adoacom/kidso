6cd535f2ac054681f2bd9db7b426798c36528e98
htcuong <htcuong@brights.vn>
2015-03-04 00:02:43 +0700

    #2 Main Page - Recommendations Improvement

--- editor_plugin.php
+++ editor_plugin.php
@@ -2,107 +2,53 @@
 	ini_set('display_errors',1);
 	ini_set('display_startup_errors',1);
 	error_reporting(-1);	
+	$lang = getLang();
 	$result = getEditor_plugin();
-// 	print_r($result);
+ 	
 ?>
-<script>
-function setEditorUI(){
-	$("#Photo img").load(function() {
-    	$(this).wrap(function(){
-	      return '<span class="image-wrap ' + $(this).attr('class') + '" style="position:relative; display:inline-block; background:url(' + $(this).attr('src') + ') no-repeat center center; width: 70px; height: 70px;" />';
-    	});
-    	$(this).css("opacity","0");
-	});	
-	$('.flexslider').flexslider({
-		animation: "slide",
-	    animationLoop: false,
-// 		        itemWidth: 1482,
-		itemHeight: 200,
-		slideshow: false,
-		controlsContainer: ".flex-container",
-		controlNav: false,
-// 	    directionNav: false,
-        start: function(slider) {
-			$('body').removeClass('loading');          
-            $('.total-slides').text(slider.count);
-       },
-		after: function(slider) {
-			slider.find('.slideIndex').html(slider.currentSlide + 1);
-	   }				
-	 });	
-}
-function goDetail(id){
-// 	window.location = 'detail.php?pid='+id;
-}
-</script>
-	
-	<div class="infobody">
-<!-- 	<div class="infoHead">&nbsp;</div>	 -->
-<!-- 	<div class="infoMiddle"> -->
-<!-- 
-	<table class="info">
-	<tr class="infoBG">
-		<th class="infoTD"><div id="Photo"><div class="box circle">
-		<img src="<?php echo $result['user_img'];?>" class="photo-img">
-		</div></div>
-		</th>
-		<td>
-			<center>
-			<img src="images/editor_pen.png" width="24"><br>
-			<span class="content-gray content-Text2"><?php echo $result['user'];?></span><br>
-			<span class="content-gray content-Text2"><?php echo $result['createdate'];?></span>
-			</center>
-		</td>
-	</tr>
-	</table>
- -->
-<!-- 	<div class="infoFooter">&nbsp;</div> -->
-<!-- 	</div> -->
-<style>
-.editorPlugin{
-	width:100%;
-}
-.editorPlugin th{
-	width:40%;
-	padding: 10px;
-}
-.editorPlugin td{
-	width:60%;
-	padding: 10px;	
-}
-</style>
-	<div class="flexslider">
-		<ul class="slides">
-		<?php
-			for($i=0;$i<count($result);$i++):
-		?>		
-			<li>
-			<table class="editorPlugin">
-			<tr>
-				<th class="content-top">
-				<?php
-					if(!empty($result[$i]['image']))
-					{
-						$img = explode(",",$result[$i]['image']);
-						if(count($img)>0):
-							for($y=0;$y<count($img);$y++):	
-				?>
-				  	   	    <img src="upload/<?php echo $img[$y];?>" />
-				<?php
-							endfor;
-						endif;
-					}
-				?>
-				</th>
-				<td class="content-top">
-					<div class=" content-Text3 content-left content-black toLink" onclick="javascript:goDetail(<?php echo $result[$i]['editor_id'];?>);"><?php echo limit_text(autoLang($result[$i]['description_ch'],$result[$i]['description_en']),550);?></div>	
-				</td>
-			</tr>
-			</table>
-			</li>		   				
+
+<div class="list_editor">
 	<?php
-		endfor;
+		foreach($result as $key => $item):
+	
+			$title = $item['title_'.$lang];
+			$description = $item['description_'.$lang];
+			$description_arr = explode("</p>",$description);
+			$summary = $description_arr[0]."</p>" . $description_arr[1]."</p>";
+			$date = date("F j, Y",strtotime($item['createdate']));
+			$num_views = $item['view_numbers'];
+			$img = explode(",",$item['image']);
+			$user = $item['user'];
+			$editor_id = $item['editor_id'];
 	?>
-		</ul>
+	<div class="item">
+		<div class="col_left">
+			<div class="img_border">
+				<img src="upload/<?php echo $img[0];?>" />
+			</div>
+		</div>
+		<div class="col_right">
+			<h2 class="title"><?php echo $title ?></h2>
+			<span class="date"><i class="fa fa-calendar"></i> <?php echo $date?></span>
+			<div class="meta_info">
+				<div class="fb-share-button" data-href="/editor_detail.php?id=<?php echo $editor_id;?>" data-layout="button_count"></div>
+				<span>/</span>
+				<span class="num_views"><?php echo $num_views?> <i class="fa fa-eye"></i></span>
+			</div>
+			<div class="summary">
+				<?php echo $summary;?>
+			</div>
+		</div>
 	</div>
+	<?php 
+		endforeach;
+	?>
 </div>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&appId=121393444718847&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
